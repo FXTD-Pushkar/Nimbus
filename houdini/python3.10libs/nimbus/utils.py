1fc1b20d6373966ca8190db7b310eec6f8bb5b71
Pushkar Deshpandey <47152267+FXTD-Pushkar@users.noreply.github.com>
2025-10-31 16:54:34 +0000

    Delete houdini/python3.10libs/nimbus directory

--- houdini/python3.10libs/nimbus/utils.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025 Pushkar Deshpandey
-# Licensed under the MIT License. See LICENSE for details.
-
-def safe_eval_parm(node, name, default=None):
-    p = node.parm(name)
-    try:
-        return p.eval() if p else default
-    except Exception:
-        return default
