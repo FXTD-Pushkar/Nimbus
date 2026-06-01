6928d0bce6e23e1ff08f5e0482a72550a98b1932
Pushkar Deshpandey <47152267+FXTD-Pushkar@users.noreply.github.com>
2026-06-01 20:27:12 +0530

    Delete H20_5 directory

--- H20_5/scripts/python/nimbus/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Nimbus package root for H20_5 tools.
-
-This file ensures Houdini can resolve this package as a concrete package
-instead of merging namespace paths from other Houdini versions.
-"""
-
