e394320bff8d999a0c1b8135330a4215eb8f7fb2
Pushkar Deshpandey <47152267+FXTD-Pushkar@users.noreply.github.com>
2026-06-06 02:40:21 +0530

    Delete H21_0 directory

--- H21_0/scripts/python/nimbus/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Nimbus package root for H21 tools.
-
-This file ensures Houdini can resolve this package as a concrete package
-instead of merging namespace paths from other Houdini versions.
-"""
-
