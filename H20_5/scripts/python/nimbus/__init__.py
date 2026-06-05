738ac24f2f59cacd48b55e5eadd76411f6f66f28
Pushkar Deshpandey <47152267+FXTD-Pushkar@users.noreply.github.com>
2026-06-06 02:41:06 +0530

    Add files via upload

---
+++ H20_5/scripts/python/nimbus/__init__.py
@@ -0,0 +1,7 @@
+"""
+Nimbus package root for H20_5 tools.
+
+This file ensures Houdini can resolve this package as a concrete package
+instead of merging namespace paths from other Houdini versions.
+"""
+
